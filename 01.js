5cfbe7c1725aeb2dddfd12c3f92879e66f79723f
Miquel Fire Burns <miquelfire@gmail.com>
2022-12-01 11:08:48 -0500

    Day 1 done.

--- 01.js
+++ 01.js
@@ -1,9 +1,27 @@
-export const part1 = async (d: string) => {
-	const data = d.split('\n\n').map(e => e.split('\n').map(e => parseInt(e, 10)));
-	return data;
+/**
+ * @param {string} d 
+ */
+export const part1 = async d => {
+	const data = d.split('\n\n')
+		.map(e => e.split('\n')
+			.map(e => parseInt(e, 10))
+			.reduce((p, c) => p + c, 0)
+		);
+	return Math.max(...data);
 };
 
-export const part2 = async (d: string) => {
-	const data = d.split('\n');
+/**
+ * @param {string} d 
+ */
+export const part2 = async d => {
+	const data = d.split('\n\n')
+		.map(
+			e => e.split('\n')
+				.map(e => parseInt(e, 10))
+				.reduce((p, c) => p + c, 0)
+		)
+		.sort((a,b)=> b - a)
+		.slice(0, 3)
+		.reduce((p, v) => p + v, 0);
 	return data;
 };
