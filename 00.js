5450d110ade4bf287b483543f95461537a92b2a7
Miquel Fire Burns <miquelfire@gmail.com>
2022-12-01 11:08:34 -0500

    Forgot to change this back to JS from TS

--- 00.js
+++ 00.js
@@ -1,9 +1,16 @@
-export const part1 = async (d: string) => {
+'use strict';
+/**
+ * @param {string} d 
+ */
+export const part1 = async d => {
 	const data = d.split('\n');
 	return data;
 };
 
-export const part2 = async (d: string) => {
+/**
+ * @param {string} d 
+ */
+export const part2 = async d => {
 	const data = d.split('\n');
 	return data;
 };
