f2b69eceb6e3fd4c9a5d760c0f65bcf52409eca2
Miquel Fire Burns <miquelfire@gmail.com>
2022-12-02 00:28:20 -0500

    Day 2 Part 1 Done

---
+++ 02.js
@@ -0,0 +1,27 @@
+'use strict';
+/**
+ * @param {string} d 
+ */
+export const part1 = async d => {
+	const winData = {
+		'A X': 1 + 3,
+		'A Y': 2 + 6,
+		'A Z': 3 + 0,
+		'B X': 1 + 0,
+		'B Y': 2 + 3,
+		'B Z': 3 + 6,
+		'C X': 1 + 6,
+		'C Y': 2 + 0,
+		'C Z': 3 + 3,
+	};
+	const data = d.split('\n').map(e => winData[e]).reduce((p, c) => p + c, 0);
+	return data;
+};
+
+/**
+ * @param {string} d 
+ */
+export const part2 = async d => {
+	const data = d.split('\n');
+	return data;
+};
