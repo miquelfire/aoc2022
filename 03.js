7c21f999d7927a70ccae0438405657490c98c2b7
Miquel Fire Burns <miquelfire@gmail.com>
2022-12-03 00:46:45 -0500

    Day 3 Complete

--- 03.js
+++ 03.js
@@ -26,6 +26,40 @@ export const part1 = async d => {
  * @param {string} d 
  */
 export const part2 = async d => {
-	const data = d.split('\n');
+	const data = d
+		.split('\n')
+		.map(e => {
+			return [... new Set(e.split(''))];
+		})
+		.reduce((result, item, index) => {
+			const chunkIndex = Math.floor(index/3);
+
+			if (!result[chunkIndex]) {
+				result[chunkIndex] = []
+			}
+
+			result[chunkIndex].push(item);
+
+			return result;
+		}, [])
+		.map(e => {
+			let [first, ...remain] = e;
+			remain.map(s => {
+				first = first.filter(p => s.includes(p));
+			});
+
+			return first;
+		})
+		.flat()
+		.map(e => {
+			e = e.codePointAt(0);
+			if (e > 96) {
+				return e - 96;
+			} else {
+				return e - 38;
+			}
+		})
+		.reduce((p, v) => p + v, 0)
+		;
 	return data;
 };
