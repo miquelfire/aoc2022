8496c0afe76b434f8b42d1e0c3aa2c5656614c70
Miquel Fire Burns <miquelfire@gmail.com>
2022-12-04 00:34:36 -0500

    Day 4 part 1

---
+++ 04.js
@@ -0,0 +1,33 @@
+'use strict';
+/**
+ * @param {string} d 
+ */
+export const part1 = async d => {
+	const data = d.split('\n')
+		.map(e => e.split(',')
+			.map(e => e.split('-').map(e => parseInt(e, 10)))
+		);
+	data.forEach((v, index) => {
+		if (v[0][0] >= v[1][0] && v[0][1] <= v[1][1]) {
+			data[index] = true;
+		}
+		else if (v[1][0] >= v[0][0] && v[1][1] <= v[0][1]) {
+			data[index] = true;
+		}
+	});
+	return data.filter(e => e === true).length;
+};
+
+/**
+ * @param {string} d 
+ */
+export const part2 = async d => {
+	const data = d.split('\n')
+		.map(e => e.split(',')
+			.map(e => e.split('-').map(e => parseInt(e, 10)))
+		);
+	data.forEach((v, index) => {
+		
+	});
+	return data;
+};
