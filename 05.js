6f7c6ffbf460901259da1df00b4f2e5a1bae8d1a
Miquel Fire Burns <miquelfire@gmail.com>
2022-12-05 00:49:05 -0500

    Part 1 Done

--- 05.js
+++ 05.js
@@ -2,14 +2,37 @@
  * @param {string} d 
  */
 export const part1 = async d => {
-	const data = d.split('\n');
-	return data;
+	const crateStack = [];
+	const data = d.split('\n\n').map(e=> e.split('\n'));
+	const columns = parseInt(data[0].pop().match(/(\d)/g).pop(), 10);
+	for(let i = 0; i < columns; i++) {
+		crateStack.push([]);
+	}
+	data[0] = data[0]
+		.map(e => [...e.matchAll(/[ []([A-Z ])[\] ] ?/g)].map(e=> e[1]))
+		.reverse();
+	data[0].forEach(row => {
+		row.forEach((crate, column) => {
+			if (crate !== ' ') {
+				crateStack[column].push(crate);
+			}
+		});
+	});
+	
+	data[1].forEach(command => {
+		const moves = command.match(/move (\d+) from (\d+) to (\d+)/).map(e => parseInt(e, 10));
+		for (let i = 0; i < moves[1]; i++) {
+			const crate = crateStack[moves[2] - 1].pop();
+			crateStack[moves[3] - 1].push(crate);
+		}
+	});
+	return crateStack.reduce((p,v) => p + v.pop(), '');
 };
 
 /**
  * @param {string} d 
  */
 export const part2 = async d => {
-	const data = d.split('\n');
+	const data = d.split('\n\n');
 	return data;
 };
