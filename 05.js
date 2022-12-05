3270d2fc1d45a31005ca3c8c1b669a6e398c426d
Miquel Fire Burns <miquelfire@gmail.com>
2022-12-05 00:52:57 -0500

    Day 5 done

--- 05.js
+++ 05.js
@@ -33,6 +33,27 @@ export const part1 = async d => {
  * @param {string} d 
  */
 export const part2 = async d => {
-	const data = d.split('\n\n');
-	return data;
+	const crateStack = [];
+	const data = d.split('\n\n').map(e=> e.split('\n'));
+	const columns = parseInt(data[0].pop().match(/(\d)/g).pop(), 10);
+	for(let i = 0; i < columns; i++) {
+		crateStack.push([]);
+	}
+	data[0] = data[0]
+		.map(e => [...e.matchAll(/[ []([A-Z ])[\] ] ?/g)].map(e=> e[1]))
+		.reverse();
+	data[0].forEach(row => {
+		row.forEach((crate, column) => {
+			if (crate !== ' ') {
+				crateStack[column].push(crate);
+			}
+		});
+	});
+	
+	data[1].forEach(command => {
+		const moves = command.match(/move (\d+) from (\d+) to (\d+)/).map(e => parseInt(e, 10));
+		const crates = crateStack[moves[2] - 1].splice(-moves[1]);
+		crateStack[moves[3] - 1].push(...crates);
+	});
+	return crateStack.reduce((p,v) => p + v.pop(), '');
 };
