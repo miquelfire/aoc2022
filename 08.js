7b22548fca40d6357e9b5bce6c6ee87172a434ef
Miquel Fire Burns <miquelfire@gmail.com>
2022-12-08 18:03:19 -0500

    Day 8 part 1 done

--- 08.js
+++ 08.js
@@ -1,3 +1,4 @@
+import { arrayBuffer } from 'stream/consumers';
 import { deepCopy } from './utils.js';
 
 /**
@@ -5,17 +6,32 @@ import { deepCopy } from './utils.js';
  */
 export const part1 = async d => {
 	const data = d.split('\n').map(e => e.split('').map(e => parseInt(e, 10)));
+	const dataTranspose = data[0].map((_, colIndex) => data.map(row => row[colIndex]));
+	/** @type {number[][]} */
 	const ret = deepCopy(data);
 
 	// Loop inside the trees
 	for (let x = 1; x < data.length - 1; x++) {
 		for (let y = 1; y < data[x].length - 1; y++) {
+			// Set to invisible
+			ret[x][y] = false;
 			// Do checks if visible
 
 			// Check row
+			if (dataTranspose[y].slice(0, x).filter(e => e >= data[x][y]).length == 0) {
+				ret[x][y] = true;
+			}
+			if (dataTranspose[y].slice(x + 1).filter(e => e >= data[x][y]).length == 0) {
+				ret[x][y] = true;
+			}
 
 			// Check column
-			console.log(data[x][y]);
+			if (data[x].slice(0, y).filter(e => e >= data[x][y]).length == 0) {
+				ret[x][y] = true;
+			}
+			if (data[x].slice(y + 1).filter(e => e >= data[x][y]).length == 0) {
+				ret[x][y] = true;
+			}
 		}
 	}
 
@@ -28,7 +44,7 @@ export const part1 = async d => {
 		ret[0][i] = true;
 		ret[ret.length - 1][i] = true;
 	}
-	return ret;
+	return ret.flat().filter(e => e).length;
 };
 
 /**
